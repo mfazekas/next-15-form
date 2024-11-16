fdcc7fbb8d5d04362d3142fc87e4ddeb92b788db
Miklós Fazekas <mfazekas@szemafor.com>
2024-11-16 21:02:52 +0100

    React hook form with nextjs 15

---
+++ app/react-hook-form-c.jsx
@@ -0,0 +1,21 @@
+"use client"
+import { useForm } from 'react-hook-form';
+
+export function ReactHookForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))} className="bg-gray-100 p-4 rounded-md flex flex-col">
+      <input {...register('firstName')} className="m-2 p-1 rounded-sm border border-gray-700" />
+      <input {...register('lastName', { required: true })} className="m-2 p-1 rounded-sm border border-gray-700" />
+      {errors.lastName && <p className="mx-2 text-red-600">Last name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} className="m-2 p-1 rounded-sm border border-gray-700" />
+      {errors.age && <p className="mx-2 text-red-600">Please enter number for age.</p>}
+      <input type="submit"  className="m-2 p-1 rounded-sm border bg-gray-300" />
+    </form>
+  );
+}
---
+++ app/react-hook-form/page.tsx
@@ -0,0 +1,13 @@
+import { ReactHookForm } from "../react-hook-form-c";
+
+
+export default function Page() {
+  return (
+    <>
+      <div className="container mx-auto px-10 mt-10">
+
+          <ReactHookForm />
+      </div>
+    </>
+  )
+}
